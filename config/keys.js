1bde6e34a3eca9bbc55a064c09f13b8e0454c857
Dayalan <Rajesh.Dayalan@agilysys.com>
2019-07-09 15:35:39 +0530

    connection changes to mongo DB

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb+srv://rajesh2701:'+ encodeURIComponent('27011996@Bday') + '@testcluster-ixc9b.mongodb.net/test?retryWrites=true';
 
 module.exports = {
     mongoURI: dbPassword
